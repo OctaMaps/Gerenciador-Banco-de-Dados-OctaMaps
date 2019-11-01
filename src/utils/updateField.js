2eeb38555cf2360c9a25de373513f34a84f5a666
teijiw <teijiwatanabe01@gmail.com>
2019-11-01 16:40:55 -0400

    Ajuste no funcionado do updateField

--- src/utils/updateField.js
+++ src/utils/updateField.js
@@ -2,7 +2,11 @@ const updateField = async (event, state) => {
 	const { name, value } = event.target
 	const valueHandle = value.trim()
 	const itemState = { ...state }
-	itemState[name] = valueHandle
+	if (valueHandle.length < 1) {
+		itemState[name] = valueHandle
+	} else {
+		itemState[name] = value
+	}
 	return itemState
 }
 
