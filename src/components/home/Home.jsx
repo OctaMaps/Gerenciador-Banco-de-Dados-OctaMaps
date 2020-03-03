7e921f78ba0acfdec92ffbaa8d89f9d0208fd10d
TeijiW <teijiwatanabe01@gmail.com>
2020-03-03 02:38:09 -0400

    Implementação de Dockerfile e nginx.conf

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,18 +3,23 @@ import Main from "../templates/Main"
 
 export default props => {
 	return (
-		<Main icon="home" title="Inicio" subtitle="Sistema de Gestão do Octa Maps">
+		<Main
+			icon="home"
+			title="Inicio"
+			subtitle="Sistema de Gestão do Octa Maps"
+		>
 			<div className="display-4">Bem-Vindo {props.name}</div>
 			<p>
 				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry. Lorem Ipsum has been the industry's standard dummy text ever
-				since the 1500s, when an unknown printer took a galley of type and
-				scrambled it to make a type specimen book. It has survived not only five
-				centuries, but also the leap into electronic typesetting, remaining
-				essentially unchanged. It was popularised in the 1960s with the release
-				of Letraset sheets containing Lorem Ipsum passages, and more recently
-				with desktop publishing software like Aldus PageMaker including versions
-				of Lorem Ipsum.
+				industry. Lorem Ipsum has been the industry's standard dummy
+				text ever since the 1500s, when an unknown printer took a galley
+				of type and scrambled it to make a type specimen book. It has
+				survived not only five centuries, but also the leap into
+				electronic typesetting, remaining essentially unchanged. It was
+				popularised in the 1960s with the release of Letraset sheets
+				containing Lorem Ipsum passages, and more recently with desktop
+				publishing software like Aldus PageMaker including versions of
+				Lorem Ipsum.
 			</p>
 		</Main>
 	)
